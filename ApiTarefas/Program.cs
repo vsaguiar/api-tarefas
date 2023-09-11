29659238f6c7efe443317551102872c9e7b91e5c
Vinicius Silva <vsaguiar@outlook.com>
2023-09-11 03:44:02 -0300

    Definições de mapeamento GET e POST

--- ApiTarefas/Program.cs
+++ ApiTarefas/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Registando o contexto como um serviço no container de injeção de dependência
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("TarefasDB"));
 
 var app = builder.Build();
 
@@ -12,6 +17,37 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+#region Definições de mapeamento
+
 app.MapGet("/", () => "Olá, mundo! ");
 
+app.MapGet("/tarefas", async (AppDbContext db) => { return await db.Tarefas.ToListAsync(); });
+
+app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
+{
+    db.Tarefas.Add(tarefa);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
+});
+
+#endregion
+
 app.Run();
+
+
+// Modelo de domínio
+class Tarefa
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public bool IsConcluida { get; set; }
+}
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { }
+
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+}
